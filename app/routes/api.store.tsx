1c359897b54c4809803e14194ffd18d537f66197
tuvaisaksen1-wq <tuvaisaksen1@gmail.com>
2026-03-04 15:51:16 +0100

    Update api.store.tsx

--- app/routes/api.store.tsx
+++ app/routes/api.store.tsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import prisma from "../db.server";
+import prisma from "../server/db.server";
 
 export async function loader() {
   const store = await prisma.store.findFirst();
